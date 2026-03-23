1342cf82ce6395cc7c7986905d6e79ba6a486dec
Geraldo <geraldobsn@gmail.com>
2026-03-23 11:18:16 -0300

    23032026 - Geraldo Neto criação de variaveis de login

---
+++ processoLogin.php
@@ -0,0 +1,12 @@
+<?php
+//Criado por Geraldo Neto - 23032026
+
+//Recebendo Usuario via POST da página de Login
+$email = $_POST['email'];
+print $email;
+
+//Recebendo Senha via POST da página de Login
+$senha = $_POST['senha'];
+print $senha;
+
+?>
