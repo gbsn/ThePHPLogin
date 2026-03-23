f425495bfbe355e3e9f9604055522f160d1ee715
Geraldo <geraldobsn@gmail.com>
2026-03-23 11:39:54 -0300

    23032026 - Geraldo Neto Atualização final login.html e processoPhp.php

--- processoLogin.php
+++ processoLogin.php
@@ -3,10 +3,34 @@
 
 //Recebendo Usuario via POST da página de Login
 $email = $_POST['email'];
-print $email;
 
 //Recebendo Senha via POST da página de Login
 $senha = $_POST['senha'];
-print $senha;
 
+//Recebendo Senha via POST da página de Login
+$senha2 = $_POST['senha2'];
+
+//Verificando variáveis
+print "Login: ".$email. " Senha: ".$senha. " Senha Comparação: ".$senha2;
+
+// Função para validar as senhas
+function validarSenhas($s1, $s2) {
+    if ($s1 === $s2) {
+        echo "<h3>Senhas Conferem!</h3>";
+        return true;
+    } else {
+        echo "<h3>Erro: As senhas não são iguais.</h3>";
+        return false;
+    }
+}
+
+// Executando a validação antes de prosseguir
+if (validarSenhas($senha, $senha2)) {
+    // Aqui entrará o seu código de inserção no Banco de Dados (BD)
+    echo "Prosseguindo para a gravação no banco...";
+} else {
+        // O redirecionamento precisa acontecer ANTES de qualquer comando 'echo' ou 'print'
+        header("Location: login.html?erro=senha_invalida");
+        exit(); // Interrompe a execução do script para garantir o redirecionamento
+    }
 ?>
